356324fa0f6f7379839d09427e6af019c6013ac6
prateekvyas git config --global user.name <prateekvyas00@gmail.com>
2025-11-17 10:13:38 +0530

    signup and signin Feature added

--- OOPs_Proj.py
+++ OOPs_Proj.py
@@ -16,9 +16,9 @@ class chatbook:
                         4. Press 4 to message a friend
                         5. Press any other Key to exit""")
         if user_input == '1':
-            pass
+            self.signup()
         elif user_input == '2':
-            pass
+            self.signin()
         elif user_input == '3':
             pass
         elif user_input == '4':
@@ -27,6 +27,29 @@ class chatbook:
             exit()  
 
 
+    def signup(self):
+        email = input("Enter your email here -> ")
+        pwd = input("Enter your password here -> ")
+        self.username = email
+        self.password = pwd
+        print("User signed up successfully !!")
+        print("/n")
+        self.menu()
+
+    def signin(self):
+        if(self.username=='' and self.password==''):
+            print("Please signup first by pressing 1 in the main menu !!")
+        else:
+            uname = input("Enter your email/username here -> ")
+            pwd = input("Enter your password here -> ")
+            if self.username == uname and self.password == pwd:
+                print("You have signed in successfully !!")
+                self.loggedin = True
+            else:
+                print("please inpput correct credentials !!")
+        print("\n")
+        self.menu()
+        
 obj = chatbook()
 
 
